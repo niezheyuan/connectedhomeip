b8464216714ff3fbeaa905d834bc025c6fcf75fc
Ken Powell <ken_powell@apple.com>
2021-06-25 11:03:48 -0700

    Adding more arguments to create_error_wrapper.py

--- src/controller/python/create_error_wrapper.py
+++ src/controller/python/create_error_wrapper.py
@@ -1,19 +1,21 @@
-import optparse
 import sys
 import re
+import os
+import argparse
+
 
 class ErrorCode(object):
     def __init__(self, error_name, error_code, min_error_code):
-        self.name = error_name.replace('_',' ').title().replace(' ','')
+        self.name = error_name.replace('_', ' ').title().replace(' ', '')
         self.message = error_name
         self.code = int(error_code) + min_error_code
-    
+
     def __str__(self):
         return '<ErrorCode name="{}" code="{}">'.format(self.name, self.code)
-    
+
     def __repr__(self):
         return '<ErrorCode name="{}" code={}>'.format(self.name, self.code)
-    
+
     def create_exception_class(self):
         return '''
 struct <name> : public std::exception
@@ -23,16 +25,17 @@ struct <name> : public std::exception
     	return "<message>";
     }
 };
-'''.replace('<name>',self.name).replace('<message>',self.message)
+'''.replace('<name>', self.name).replace('<message>', self.message)
 
     def create_py_bind(self, module_name):
         return 'py::register_exception<chip::PythonBindings::{}>({}, "{}");'.format(self.name, module_name, self.name)
-    
+
     def create_else_if(self, var_name):
         return '''
     else if (<var_name> == <error_code>) {
         throw chip::PythonBindings::<name>();
-    }'''.replace('<var_name>',var_name).replace('<error_code>',str(self.code)).replace('<name>',self.name)
+    }'''.replace('<var_name>', var_name).replace('<error_code>', str(self.code)).replace('<name>', self.name)
+
 
 def generate_header(error_codes):
     structs = "".join([x.create_exception_class() for x in error_codes])
@@ -47,10 +50,12 @@ namespace PythonBindings {
 void CHIPErrorToException(CHIP_ERROR err);
 }
 }
-'''.replace('<structs>',structs)
+'''.replace('<structs>', structs)
+
 
 def generate_cpp(error_codes):
-    exception_binding = '\n\t'.join([x.create_py_bind('M("ChipExceptions")') for x in error_codes])
+    exception_binding = '\n\t'.join(
+        [x.create_py_bind('M("ChipExceptions")') for x in error_codes])
     elseifs = ''.join([x.create_else_if('err') for x in error_codes])
     return '''
 #include "pybind11/pybind11.h"
@@ -70,54 +75,88 @@ void bind_CHIPController_ChipExceptions(std::function< pybind11::module &(std::s
 
     <exception_binding>
 }
-'''.replace('<exception_binding>',exception_binding).replace('<elseifs>',elseifs)
-
-ERROR_PATH = '../../src/lib/core/CHIPError.h'
-CONFIG_PATH = '../../src/lib/core/CHIPConfig.h'
-
-error_header = None
-config_header = None
-
-with open(ERROR_PATH, 'r') as f:
-    error_header = f.read()
-
-with open(CONFIG_PATH, 'r') as f:
-    config_header = f.read()
-
-if error_header != None and config_header != None:
-    min_err_re = re.compile(r'CHIP_CONFIG_ERROR_MIN (\d+)')
-    matches = min_err_re.findall(config_header)
-
-    min_err = -1
-
-    if len(matches) >= 1:
-        min_err = int(matches[0])
-
-    if min_err != -1:
-        error_code_re = re.compile(r'#define (\S+).*_CHIP_ERROR\((\d+)\)')
-        error_code_tuples = error_code_re.findall(error_header)
-        error_codes = [ErrorCode(x[0],x[1], min_err) for x in error_code_tuples]
-        header = generate_header(error_codes)
-        cpp = generate_cpp(error_codes)
+'''.replace('<exception_binding>', exception_binding).replace('<elseifs>', elseifs)
 
 
-def main(argv):
-    parser = optparse.OptionParser()
-    parser.add_option('--output_cpp_file')
-    parser.add_option('--output_header_file')
+def create_error_bindings(error_path, config_path, header_out, cpp_out):
+    error_header = None
+    config_header = None
+
+    with open(error_path, 'r') as f:
+        error_header = f.read()
+
+    with open(config_path, 'r') as f:
+        config_header = f.read()
+
+    if error_header != None and config_header != None:
+        min_err_re = re.compile(r'CHIP_CONFIG_ERROR_MIN (\d+)')
+        matches = min_err_re.findall(config_header)
+
+        min_err = -1
+
+        if len(matches) >= 1:
+            min_err = int(matches[0])
+
+        if min_err != -1:
+            error_code_re = re.compile(r'#define (\S+).*_CHIP_ERROR\((\d+)\)')
+            error_code_tuples = error_code_re.findall(error_header)
+            error_codes = [ErrorCode(x[0], x[1], min_err)
+                           for x in error_code_tuples]
+            header = generate_header(error_codes)
+            cpp = generate_cpp(error_codes)
+
+            print("Creating Header file: {}".format(header_out))
+            with open(header_out, 'w') as f:
+                f.write(header)
+
+            print("Creating CPP file {}".format(cpp_out))
+            with open(cpp_out, 'w') as f:
+                f.write(cpp)
+
+        else:
+            print("Could not find min error value")
+            sys.exit(1)
+    else:
+        print("Unable to open required files :(")
+        sys.exit(1)
 
 
-    options, _ = parser.parse_args(argv)
+def main():
+    p = argparse.ArgumentParser()
 
-    with open(options.output_cpp_file, 'w') as output_cpp_file:
-        output_cpp_file.write(cpp)
+    p.add_argument('--output_cpp_file', required=True,
+                   help='Output location for .cpp file')
+    p.add_argument('--output_header_file', required=True,
+                   help='Output location for .h file')
+    p.add_argument('--error_header', required=False,
+                   help='Location of Error Header file (ex CHIPError.h)')
+    p.add_argument('--config_header', required=False,
+                   help='Location of Config Header file (ex CHIPConfig.h)')
 
-    with open(options.output_header_file, 'w') as output_header_file:
-        output_header_file.write(header)
+    args = p.parse_args()
 
+    output_cpp_file = args.output_cpp_file
+    output_header_file = args.output_header_file
 
-    return 0
+    error_path = '../../lib/core/CHIPError.h'
+    config_path = '../../lib/core/CHIPConfig.h'
+
+    if args.error_header != None and args.error_header != "":
+        error_path = os.path.abspath(args.error_path)
+
+    if not os.path.exists(error_path):
+        print("Error Header Path {} does not exist".format(error_path))
+        sys.exit(1)
+
+    if args.config_header != None and args.config_header != "":
+        config_path = os.path.abspath(args.config_header)
+
+    if not os.path.exists(config_path):
+        print("Config Header Path {} does not exist!".format(config_path))
+
+    create_error_bindings(error_path, config_path,
+                          output_header_file, output_cpp_file)
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    main()
